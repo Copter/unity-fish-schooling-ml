94c79ef2c59681b0084678d22a67e1036f9b6ae0
Copter <copter12349@hotmail.com>
2022-01-28 00:48:46 +0700

    Predator played by simple CPU

--- UnityProject/Assets/Scripts/sharkScript.cs
+++ UnityProject/Assets/Scripts/sharkScript.cs
@@ -8,6 +8,11 @@ public class sharkScript : MonoBehaviour
 	public float swimSpeed = 25f;
 	Vector2 initialVector;
 	public float steerStrength = 2.5f;
+    public bool playerControlled = false;
+    public float rayLength = 42f;
+    public float rayThickness = 20f;
+    public LayerMask rayLayerMask;
+
 
     //public RayPerceptionSensorComponent2D rays;
 
@@ -23,26 +28,99 @@ public class sharkScript : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        // TO-DO:
-        if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
-        {
-            Vector2 steerDirVector = new Vector2(-rb.velocity.y, rb.velocity.x);
-            steerDirVector = steerDirVector.normalized * steerStrength;
-            rb.velocity = steerDirVector + (swimSpeed * rb.velocity.normalized);
-        }
-        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
-        {
-            Vector2 steerDirVector = new Vector2(rb.velocity.y, -rb.velocity.x);
-            steerDirVector = steerDirVector.normalized * steerStrength;
-            rb.velocity = steerDirVector + (swimSpeed * rb.velocity.normalized);
+        if(playerControlled){
+            if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D))
+            {
+                Vector2 steerDirVector = new Vector2(-rb.velocity.y, rb.velocity.x);
+                steerDirVector = steerDirVector.normalized * steerStrength;
+                rb.velocity = steerDirVector + (swimSpeed * rb.velocity.normalized);
+            }
+            else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+            {
+                Vector2 steerDirVector = new Vector2(rb.velocity.y, -rb.velocity.x);
+                steerDirVector = steerDirVector.normalized * steerStrength;
+                rb.velocity = steerDirVector + (swimSpeed * rb.velocity.normalized);
+            }
+            else
+            {
+                rb.velocity = swimSpeed * rb.velocity.normalized;
+            }
 
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, rb.velocity);
         }
-        else
-        {
+
+        Debug.DrawRay(transform.position, transform.TransformDirection(Vector2.up) * rayLength);
+
+        //RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.up), rayLength, rayLayerMask);    // ~LayerMask.NameToLayer ("Food")
+        RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, rayThickness, transform.TransformDirection(Vector2.up), rayLength, rayLayerMask);
+
+        if (hits.Length > 0) {
+            RaycastHit2D hit = hits[0];
+            
+            if (hit) {
+                //print("Raycast hit: " + hit.collider.name);
+
+                if(hit.collider.gameObject.tag == "agent")
+                {
+                    //print("Target acquired: " + hit.collider.name);
+                    Transform TargetObjTransform = hit.collider.gameObject.transform;
+
+                    var relativePoint = transform.InverseTransformPoint(TargetObjTransform.position);
+                    if (relativePoint.x < -1f) 
+                    {
+                        print (hit.collider.name + " is to the left");
+                        Vector2 steerDirVector = new Vector2(-rb.velocity.y, rb.velocity.x);
+                        steerDirVector = steerDirVector.normalized * steerStrength;
+                        rb.velocity = steerDirVector + (swimSpeed * rb.velocity.normalized);
+                    } 
+                    else if (relativePoint.x > 1f) 
+                    {
+                        print (hit.collider.name + " is to the right");
+                        Vector2 steerDirVector = new Vector2(rb.velocity.y, -rb.velocity.x);
+                        steerDirVector = steerDirVector.normalized * steerStrength;
+                        rb.velocity = steerDirVector + (swimSpeed * rb.velocity.normalized);
+                    } 
+                    else 
+                    {
+                        print (hit.collider.name + " is directly ahead");
+                        rb.velocity = swimSpeed * rb.velocity.normalized;
+                    }
+
+                    transform.rotation = Quaternion.LookRotation(Vector3.forward, rb.velocity);
+                }
+                else if(hit.collider.gameObject.tag == "wall")
+                {
+                    //if(hit.distance <= 10f) {
+                    print (hit.collider.name + " ahead, avoiding it.");
+
+                    var relativePoint = transform.InverseTransformPoint(Vector3.zero);
+                    if (relativePoint.x < -1f) 
+                    {
+                        Vector2 steerDirVector = new Vector2(-rb.velocity.y, rb.velocity.x);
+                        steerDirVector = steerDirVector.normalized * steerStrength;
+                        rb.velocity = steerDirVector + (swimSpeed * rb.velocity.normalized);
+                    } 
+                    else if (relativePoint.x > 1f) 
+                    {
+                        Vector2 steerDirVector = new Vector2(rb.velocity.y, -rb.velocity.x);
+                        steerDirVector = steerDirVector.normalized * steerStrength;
+                        rb.velocity = steerDirVector + (swimSpeed * rb.velocity.normalized);
+                    } 
+                    else 
+                    {
+                        rb.velocity = swimSpeed * rb.velocity.normalized;
+                    }
+
+                    transform.rotation = Quaternion.LookRotation(Vector3.forward, rb.velocity);
+                    //}
+                }
+            }
+        }
+
+        if(!playerControlled){
             rb.velocity = swimSpeed * rb.velocity.normalized;
+            transform.rotation = Quaternion.LookRotation(Vector3.forward, rb.velocity);
         }
-
-        transform.rotation = Quaternion.LookRotation(Vector3.forward, rb.velocity);
     }
 
     public Vector2 RandomUnitVector()
