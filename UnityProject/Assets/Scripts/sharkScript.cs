5201c9ae60954cd9e9d3572e657567ac38bad9f3
Copter <copter12349@hotmail.com>
2022-02-08 22:31:28 +0700

    Shark is now less scared of walls

--- UnityProject/Assets/Scripts/sharkScript.cs
+++ UnityProject/Assets/Scripts/sharkScript.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+
 
 public class sharkScript : MonoBehaviour
 {
@@ -12,6 +14,9 @@ public class sharkScript : MonoBehaviour
     public float rayLength = 42f;
     public float rayThickness = 20f;
     public LayerMask rayLayerMask;
+    RaycastHit2D priorityHit;
+    public bool editTankCenterPos = false;
+    public Vector3 tankCenterPos;
 
 
     //public RayPerceptionSensorComponent2D rays;
@@ -23,6 +28,8 @@ public class sharkScript : MonoBehaviour
         initialVector = RandomUnitVector();
 		rb.velocity = swimSpeed * initialVector;
         //rays = GetComponent<RayPerceptionSensorComponent2D>();
+        
+        if(!editTankCenterPos) tankCenterPos = transform.position;
     }
 
     // Update is called once per frame
@@ -54,46 +61,62 @@ public class sharkScript : MonoBehaviour
         //RaycastHit2D hit = Physics2D.Raycast(transform.position, transform.TransformDirection(Vector2.up), rayLength, rayLayerMask);    // ~LayerMask.NameToLayer ("Food")
         RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, rayThickness, transform.TransformDirection(Vector2.up), rayLength, rayLayerMask);
 
-        if (hits.Length > 0) {
-            RaycastHit2D hit = hits[0];
+        if (hits.Length > 0) 
+        {
+            //RaycastHit2D hit = hits[0];
             
-            if (hit) {
-                //print("Raycast hit: " + hit.collider.name);
-
+            foreach(RaycastHit2D hit in hits)
+            {
                 if(hit.collider.gameObject.tag == "agent")
                 {
-                    //print("Target acquired: " + hit.collider.name);
-                    Transform TargetObjTransform = hit.collider.gameObject.transform;
+                    priorityHit = hit;
+                    break;
+                }
+                else if(hit.collider.gameObject.tag == "wall"/* && priorityHit.collider.gameObject.tag != "agent"*/)
+                {
+                    priorityHit = hit;
+                }
+            }
+
+            
+            //if (hit) 
+            //{
+                //print("Raycast hit: " + hit.collider.name);
+
+                if(priorityHit.collider.gameObject.tag == "agent")
+                {
+                    //print("Target acquired: " + priorityhit.collider.name);
+                    Transform TargetObjTransform = priorityHit.collider.gameObject.transform;
 
                     var relativePoint = transform.InverseTransformPoint(TargetObjTransform.position);
                     if (relativePoint.x < -1f) 
                     {
-                        print (hit.collider.name + " is to the left");
+                        print (priorityHit.collider.name + " is to the left");
                         Vector2 steerDirVector = new Vector2(-rb.velocity.y, rb.velocity.x);
                         steerDirVector = steerDirVector.normalized * steerStrength;
                         rb.velocity = steerDirVector + (swimSpeed * rb.velocity.normalized);
                     } 
                     else if (relativePoint.x > 1f) 
                     {
-                        print (hit.collider.name + " is to the right");
+                        print (priorityHit.collider.name + " is to the right");
                         Vector2 steerDirVector = new Vector2(rb.velocity.y, -rb.velocity.x);
                         steerDirVector = steerDirVector.normalized * steerStrength;
                         rb.velocity = steerDirVector + (swimSpeed * rb.velocity.normalized);
                     } 
                     else 
                     {
-                        print (hit.collider.name + " is directly ahead");
+                        print (priorityHit.collider.name + " is directly ahead");
                         rb.velocity = swimSpeed * rb.velocity.normalized;
                     }
 
                     transform.rotation = Quaternion.LookRotation(Vector3.forward, rb.velocity);
                 }
-                else if(hit.collider.gameObject.tag == "wall")
+                else if(priorityHit.collider.gameObject.tag == "wall")
                 {
-                    //if(hit.distance <= 10f) {
-                    print (hit.collider.name + " ahead, avoiding it.");
+                    //if(priorityHit.distance <= 10f) {
+                    print (priorityHit.collider.name + " ahead, avoiding it.");
 
-                    var relativePoint = transform.InverseTransformPoint(Vector3.zero);
+                    var relativePoint = transform.InverseTransformPoint(tankCenterPos); //Vector3.zero
                     if (relativePoint.x < -1f) 
                     {
                         Vector2 steerDirVector = new Vector2(-rb.velocity.y, rb.velocity.x);
@@ -114,7 +137,7 @@ public class sharkScript : MonoBehaviour
                     transform.rotation = Quaternion.LookRotation(Vector3.forward, rb.velocity);
                     //}
                 }
-            }
+            //}
         }
 
         if(!playerControlled){
